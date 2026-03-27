8ea2fe29e7a8055db81880bc8a2ffce944b8adfa
Geert van Ieperen <gvanieperen@greefa.nl>
2026-03-27 16:25:01 +0100

    Generalize JarReader, move it to Tools

--- src/io/github/ieperen3039/ngn/Tools/JarReader.java
+++ src/io/github/ieperen3039/ngn/Tools/JarReader.java
@@ -1,6 +1,4 @@
-package io.github.ieperen3039.ngn.Core;
-
-import io.github.ieperen3039.ngn.Tools.Logger;
+package io.github.ieperen3039.ngn.Tools;
 
 import java.io.File;
 import java.io.IOException;
@@ -27,9 +25,6 @@ public final class JarReader<T> {
     public JarReader(Path path, Class<T> classType) throws Exception{
         this.classType = classType;
         files = path.toFile().listFiles();
-        if (files == null || files.length == 0) {
-            throw new Exception("No scripts found in " + path);
-        }
 
         URL[] urls = new URL[files.length];
         for (int i = 0; i < files.length; i++) {
@@ -44,22 +39,21 @@ public final class JarReader<T> {
     }
 
     /**
-     * loads a jarfile, returning all {@link T} instances
+     * loads a specific jarfile, and loads all classes of type T
      * @param jarFile the jarfile to search
      * @param loader  a class loader that has access to the classes in the file
-     * @return a list of the scripts
+     * @return one instance of every classes found in the jar file that implement {@link T}
      * @throws IOException                  if the jar could not be properly loaded
      * @throws ReflectiveOperationException if the Classloader is nat able to load the classes in this file, or if the
      *                                      script does not have a constructor with one Game parameter
      * @throws IllegalArgumentException     if the provided file does not exist or is not a jar file
      */
-    public List<T> loadScripts(File jarFile, ClassLoader loader) throws IOException, ReflectiveOperationException, IllegalArgumentException {
+    public List<T> loadFile(File jarFile) throws IOException, ReflectiveOperationException {
         if (jarFile == null || !jarFile.exists()) {
-            throw new IllegalArgumentException("Invalid jar file provided");
+            throw new IOException("Invalid jar file provided: " + jarFile);
         }
-        Logger.DEBUG.print("Loading scripts from of file " + jarFile);
 
-        List<Class<?>> classes = loadClassesFromJar(jarFile, loader);
+        List<Class<?>> classes = loadClassesFromJar(jarFile, classLoader);
 
         List<T> list = new ArrayList<>(classes.size());
         for (Class<?> c : classes) {
@@ -72,8 +66,8 @@ public final class JarReader<T> {
             }
         }
 
-        int nOfScripts = list.size();
-        Logger.DEBUG.printf("%d %s found in %s", nOfScripts, (nOfScripts == 1) ? "script" : "scripts", jarFile.getName());
+        int nrOfClasses = list.size();
+        Logger.DEBUG.printf("%d %s found in %s", nrOfClasses, (nrOfClasses == 1) ? "class" : "classes", jarFile.getName());
         return list;
     }
 
@@ -81,24 +75,25 @@ public final class JarReader<T> {
      * loads all classes in all jars in the given directory, collecting all implementations of {@link T}.
      * @return a list of all loaded scripts
      */
-    public List<T> loadScripts() throws IOException {
-        Logger.DEBUG.print("Start loading scripts...");
-        List<T> scripts = new ArrayList<>();
+    public List<T> loadPlugins() throws IOException {
+        String tName = classType.getCanonicalName();
+        List<T> instances = new ArrayList<>();
         for (File jar : files) {
             try {
-                List<T> fileScripts = loadScripts(jar, classLoader);
-                scripts.addAll(fileScripts);
+                Logger.DEBUG.print("Searching " + jar.getName() + " for " + tName + "...");
+                List<T> fileScripts = loadFile(jar);
+                instances.addAll(fileScripts);
 
             } catch (ReflectiveOperationException ex) {
-                Logger.WARN.print("Could not load scripts from " + jar);
+                Logger.WARN.print("Could not load plugins from " + jar);
 
             } catch (IOException ex) {
                 Logger.WARN.print("Could not open " + jar);
             }
         }
 
-        Logger.INFO.print("Loaded " + scripts.size() + " scripts\n");
-        return scripts;
+        Logger.INFO.print("Loaded " + instances.size() + " instances of " + tName + "\n");
+        return instances;
     }
 
     /**
@@ -108,9 +103,9 @@ public final class JarReader<T> {
      * @throws IOException              If during processing of the Jar-file an error occurred
      * @throws IllegalArgumentException If either the provided file is null, does not exist or is no Jar file
      */
-    private List<Class<?>> loadClassesFromJar(File file, ClassLoader loader) throws IOException, IllegalArgumentException, ClassNotFoundException {
+    private static List<Class<?>> loadClassesFromJar(File file, ClassLoader loader) throws IOException, IllegalArgumentException, ClassNotFoundException {
         if (file == null || !file.exists())
-            throw new IllegalArgumentException("Invalid filename: " + file);
+            throw new IllegalArgumentException("Invalid file: " + file);
 
         if (!file.getName().endsWith(".jar")) {
             throw new IllegalArgumentException("Provided file was not a jar file: " + file);
